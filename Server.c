c93e5760631890e5871eed892e8d6e2ab1d8f7d5
AK <akkhan07420@gmail.com>
2022-12-03 21:16:39 +0500

    configure .gitignore file and also write some server code

--- Server.c
+++ Server.c
@@ -1,6 +1,17 @@
-#include <stdio.h>
+// Server side Socket Programming in c
+
+#include <stdio.h> // for printf
+#include <sys/socket.h> // for socket, bind, listen, recv, send
+#include <arpa/inet.h> // for sockaddr_in
 
 int main(void){
-    printf("Hello world!\n");
+    int server_socket, client_socket;
+    
+    struct sockaddr_in address; // server address
+
+    // create a server socket 
+    server_socket = socket(AF_INET, SOCK_STREAM, 0); // 0 is for protocol
+    
+
     return 0;
 }
